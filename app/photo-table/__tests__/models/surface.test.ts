109f0ef121b12890407f1e2c31256a0c4d74c485
Matt Schaefer <matt@situatedbit.com>
2022-01-20 13:48:31 -0800

    replace babel for jets in favor of swc/jest transformer

--- app/photo-table/__tests__/models/surface.test.ts
+++ app/photo-table/__tests__/models/surface.test.ts
@@ -1,5 +1,5 @@
 import { Rectangle } from './rectangle';
-import { boundingBox, Surface } from './surface';
+import { boundingBox, Surface } from '../../models/surface';
 
 describe('boundingBox', () => {
   test('any empty box has no width or height beyond margin', () => {

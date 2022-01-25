8a060b3a6446feebdb407f79547ccda63c919708
Matt Schaefer <matt@situatedbit.com>
2022-01-25 13:33:11 -0800

    image container stretches to prevent mouse from falling outside element on drag

--- app/photo-table/pages/tables/components/ImageContainer.tsx
+++ app/photo-table/pages/tables/components/ImageContainer.tsx
@@ -7,12 +7,25 @@ function ImageContainer({ image, onRemove, onMoveX, onMoveY, onMoveToTop, onMove
   const [startMoveOffset, setStartMoveOffset] = useState({ x : 0, y: 0 });
   const [midMoveOffset, setMidMoveOffset] = useState({ x: 0, y: 0 });
 
-  // Convert surface logical coordinates into CSS coordinates on surface
-  const imageContainerStyle = {
+  // Convert surface logical coordinates into CSS coordinates on surface element.
+  // By default these position properties will be applied to the entire
+  // component. When the user drags the image, the component temporarily
+  // stretches to fill the surface to prevent the mouse from falling off of this
+  // component while the tree re-renders. During that time, these position
+  // properties are applied to the inner container.
+  const positionWithinSurface = {
     left: surfaceWidth / 2 + image.x + midMoveOffset.x,
     top: surfaceHeight / 2 - image.y - midMoveOffset.y,
+  };
+
+  const imageContainerStyle = {
+    ...(isMoving ? positionWithinSurface : {}),
+  };
+
+  const draggableContainerStyle = {
     zIndex: image.zIndex,
-  }
+    ...(isMoving ? {} : positionWithinSurface),
+  };
 
   const handleMouseDown = (event: Event) => {
     // button 0 is the primary button; ignore right clicks, e.g.
@@ -55,36 +68,42 @@ function ImageContainer({ image, onRemove, onMoveX, onMoveY, onMoveToTop, onMove
     }
   };
 
+  const draggableContainerClassName = isMoving ? styles.draggableContainerDragging : styles.draggableContainer;
   const imgClassName = isMoving ? styles.imageDragging : styles.image;
 
   return (
     <div
-      className={styles.imageContainer}
-      style={imageContainerStyle}
+      className={draggableContainerClassName}
+      style={draggableContainerStyle}
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
     >
-      <img
-        className={imgClassName}
-        draggable="false"
-        height={image.height}
-        src={image.url}
-        width={image.width}
-      />
-      <form
-        className={styles.controls}
-        onSubmit={(event: Event) => event.preventDefault()}
+      <div
+        className={styles.imageContainer}
+        style={imageContainerStyle}
       >
-        <button
-          className="p-1"
-          onClick={() => onMoveToBottom(image)}
-        >⤓</button>
-        <button
-          className="p-1"
-          onClick={() => onRemove(image)}
-        >✕</button>
-      </form>
+        <img
+          className={imgClassName}
+          draggable="false"
+          height={image.height}
+          src={image.url}
+          width={image.width}
+        />
+        <form
+          className={styles.controls}
+          onSubmit={(event: Event) => event.preventDefault()}
+        >
+          <button
+            className="p-1"
+            onClick={() => onMoveToBottom(image)}
+          >⤓</button>
+          <button
+            className="p-1"
+            onClick={() => onRemove(image)}
+          >✕</button>
+        </form>
+      </div>
     </div>
   );
 }
